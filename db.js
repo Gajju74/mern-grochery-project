d2a4aafe9cdf3b4b8d7d5af92ec747a56fdae6c8
Gajju74 <gbanjara852@gmail.com>
2023-05-09 09:40:59 +0530

    some changes

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
+require("dotenv").config();
 
-const mongoURL =  process.env.DATABASE
+const mongoURL = process.env.DATABASE;
 
 
 const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("connected mongo db");
+        console.log("mongo db connected");
         
     } catch (error) {
         console.log("mongo db not connect");
